9dee71f1919c53be302b47541f70b3db22367b8e
Gudbjorg Ylfa <gugga_ylfa@hotmail.com>
2022-02-19 16:14:57 +0000

    added likes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let movieData= []
+let movieData = []
 
 fetch('https://imdb-api.com/en/API/Top250Movies/k_m41smx5l')
 .then((res)=> res.json())
@@ -7,7 +7,7 @@ fetch('https://imdb-api.com/en/API/Top250Movies/k_m41smx5l')
     funcCreateCards(movieData);
 });
 
-
+// Map function loops it to create all the different cards
 //====================================
 // Local copy of the Data
 funcLocalData = (movies) => {
@@ -41,16 +41,41 @@ funcCreateCards = (newMovieData) => {
     <div class="movieCardDetails">
       <h2>${nMD.title}</h2>
       <p>${nMD.year}</p>
-      <p>IMDB Rating: <span>${nMD.imDbRating}</span></p>
+      <p>IMDB Rating: <span>${nMD.rating}</span></p>
       <div class="movieCardSocial">
-        <a href="#"><i class="fas fa-heart"></i></a>
-        <a href="#"><i class="fas fa-share-alt"></i></a>
+        <a href="#" onClick="funcLike('${nMD.id}')"><i class="fas fa-heart ${nMD.likes ? 'likeHeart' : ''}"></i></a>
+        <a href="https://www.imdb.com/title/${nMD.id}/" target="_blank"><i class="fas fa-share-alt"></i></a>
         <a href="#"><i class="fas fa-comment"></i></a>
       </div>
     </div>`  
-    })
+    });
 
     document.querySelector('body').innerHTML = tempCardData;
 };
 
 //====================================
+
+//====================================
+// Likes Clicked
+
+funcLike = (i) => {
+    // console.log(i);
+
+   movieData =  movieData.map((m) => {
+        if (m.id === i) {
+        m.likes += 1;
+        } 
+        return m;
+    });
+
+    movieData.sort(function (a, b){
+        return b.likes - a.likes;
+    });
+    
+    funcCreateCards(movieData);
+};
+
+//====================================
+
+//====================================
+// Sorting 
